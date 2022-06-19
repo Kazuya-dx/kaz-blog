e8a8d351173633582277e458c4fe19a300ca6b0d
kaz <kachiiiscool@gmail.com>
2022-06-19 19:15:36 +0900

    仮 blog 作成

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,41 @@
 import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import fs from 'fs';
+import matter from 'gray-matter';
 import { BlogPage } from '../../features/blog';
 
-const Blog: NextPage = () => {
-  return <BlogPage />;
+type BlogProps = {
+  posts: {
+    slug: string;
+    frontmatter: {
+      [key: string]: any;
+    };
+  }[];
+};
+
+const Blog: NextPage<BlogProps> = ({ posts }) => {
+  return <BlogPage posts={posts} />;
 };
 
 export default Blog;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const files = fs.readdirSync('posts');
+
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+    const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const { data: frontmatter } = matter(readFile);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
