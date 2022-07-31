cd911f5b29d33e945ffb446b90a2b2a54b841d7f
kaz <kachiiiscool@gmail.com>
2022-07-31 14:54:22 +0900

    height 修正

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -9,8 +9,8 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <>
       <AppBar />
-      <main className="flex justify-center">
-        <div className="max-w-6xl w-full px-8 h-screen bg-white">{children}</div>
+      <main className="flex justify-center mb-48">
+        <div className="max-w-6xl w-full px-8 bg-white">{children}</div>
       </main>
       <Footer />
     </>
